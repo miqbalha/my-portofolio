0649596f2ce208cf89d61a83202b199945ad9279
miqbalha <ballemu99@gmail.com>
2022-09-27 20:05:27 +0700

    update alert

--- dist/js/script.js
+++ dist/js/script.js
@@ -55,4 +55,20 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
   dark.checked = false;
 }
 
-// // form action
+// alert remove
+{
+  /* <button
+              type="button"
+              class="btn-close -mx-1.5 -my-1.5 ml-auto inline-flex h-8 w-8 rounded-lg bg-green-100 p-1.5 text-green-500 hover:bg-green-200 focus:ring-2 focus:ring-green-400 dark:bg-green-200 dark:text-green-600 dark:hover:bg-green-300"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+            >
+              <svg aria-hidden="true" class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button> */
+}
