dee614cce51f27d4ca19c1b6889b66f71bc9c059
miqbalha <ballemu99@gmail.com>
2022-09-29 09:01:50 +0700

    update gambar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin');
 module.exports = {
-  content: ['./index.html'],
+  content: ['index.html'],
   darkMode: 'class',
   theme: {
     container: {
