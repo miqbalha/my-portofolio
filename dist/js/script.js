647d56010bf7b19220aaf9336f4718794915d1a9
miqbalha <balndutz05@gmail.com>
2023-01-29 21:11:34 +0700

    tambah skil

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,57 +1,61 @@
 //navbar fixed
 window.onscroll = function () {
-  const header = document.querySelector('header');
+  const header = document.querySelector("header");
   const fixnav = header.offsetTop;
-  const topNav = document.querySelector('#topnav');
+  const topNav = document.querySelector("#topnav");
 
   if (window.pageYOffset > fixnav) {
-    header.classList.add('navbar-fixed');
-    topNav.classList.remove('hidden');
-    topNav.classList.add('flex');
+    header.classList.add("navbar-fixed");
+    topNav.classList.remove("hidden");
+    topNav.classList.add("flex");
   } else {
-    header.classList.remove('navbar-fixed');
-    topNav.classList.remove('flex');
-    topNav.classList.add('hidden');
+    header.classList.remove("navbar-fixed");
+    topNav.classList.remove("flex");
+    topNav.classList.add("hidden");
   }
 };
 
 //hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function () {
-  hamburger.classList.toggle('hamburger-active');
-  navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
 // klik diluar navbar
-window.addEventListener('click', function (e) {
+window.addEventListener("click", function (e) {
   if (e.target != hamburger && e.target != navMenu) {
-    navMenu.classList.remove('hamburger-active');
-    navMenu.classList.add('hidden');
+    navMenu.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
   }
 });
 
 // darkmode
-const dark = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
+const dark = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
 
-dark.addEventListener('click', function () {
+dark.addEventListener("click", function () {
   if (dark.checked) {
-    html.classList.add('dark');
-    localStorage.theme = 'dark';
+    html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
-    html.classList.remove('dark');
-    localStorage.theme = 'light';
+    html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
 
 // button darkmode fixed move
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark');
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
   dark.checked = true;
 } else {
-  document.documentElement.classList.remove('dark');
+  document.documentElement.classList.remove("dark");
   dark.checked = false;
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,39 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin", "tailgrids/plugin");
 module.exports = {
-  content: ['./index.html'],
-  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./*.html",
+    "./ecommerce/*.html",
+    "./assets/**/*.js",
+  ],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
-      padding: '30px',
+      padding: "30px",
     },
     extend: {
       colors: {
-        primary: '#10b981',
-        secondary: '#64748b',
-        dark: '#0f172a',
-        igs: '#E4405F',
-        fbk: '#1877F2',
+        primary: "#10b981",
+        secondary: "#64748b",
+        dark: "#0f172a",
+        igs: "#E4405F",
+        fbk: "#1877F2",
       },
       screens: {
-        '2xl': '1320px',
+        "2xl": "1320px",
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  safelist: [
+    "!duration-0",
+    "!delay-0",
+    'html.js :where([class*="taos:"]:not(.taos-init))',
+  ],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailgrids/plugin"),
+    require("taos/plugin"),
+  ],
 };
